71fe87e202f96c3fd6b2a5397018e1bff30ecc87
vilaj46 <vilaj46@gmail.com>
2025-05-08 21:20:19 -0400

    postbuild script

--- fsp-frontend/next.config.ts
+++ fsp-frontend/next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next"
+import path from "path"
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack(config) {
+    config.resolve.alias["@shared"] = path.resolve(__dirname, "shared")
+    return config
+  },
 }
 
 export default nextConfig
