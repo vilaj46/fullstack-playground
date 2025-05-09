7e6eec62d33b74a460466adbb4c609b4f2026694
vilaj46 <vilaj46@gmail.com>
2025-05-08 20:57:25 -0400

    fix: prod bug

--- fsp-frontend/src/lib/types/HttpTypes.ts
+++ fsp-frontend/src/lib/types/HttpTypes.ts
@@ -1,5 +1,7 @@
 type TApiErrors<ExpectedErrors = undefined> = {
   message: string
-} & (ExpectedErrors extends undefined ? {} : Record<string, string>)
+} & (ExpectedErrors extends undefined
+  ? Record<string, unknown>
+  : Record<string, string>)
 
 export type { TApiErrors }
